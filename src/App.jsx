ee156dc1f397e2e062fa9ce44fe4bb14b2ac94dc
Nataly <nz_18@mail.ru>
2024-03-14 19:55:00 +0300

    Feature/create section shop (#3)
    * feat: add fonts
    
    * feat: add normalize and clsx for css classes
    
    * refactor: create section component
    
    * feat: create header and logic for burger menu
    
    * perf: add icons for header
    
    * feat: write a common sample layout
    
    * refactor: delete images copies
    
    * refactor: correct size icons and delete blue background
    
    * refactor: rename files
    
    * chore: comment code with map
    
    * refactor: add active class for slider in global css file
    
    * feat: write logic for turning slides
    
    * refactor: change margin-top to slider
    
    * feat: create section cafe
    
    * refactor: attach header
    
    * feat: write logic for season's input
    
    * feat: add animation to burger menu
    
    * refactor: add adaptivity and animation for header
    
    * feat: add favicon
    
    * feat: add profile menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import "./App.css";
 import { Header } from "./header/Header";
 import { Section } from "./shared/ui/Section/Section";
 import { Slider } from "./slider/Slider";
+import { useState } from "react";
+
 
 function App() {
+  const [season, setSeason] = useState("winter");
+
+  const handleSeasonChange = (event) => {
+    setSeason(event.target.value);
+  };
   return (
     <>
       <main>
@@ -25,41 +32,112 @@ function App() {
         </Section>
         <Section className='favorites' id='favorites'>
           <h2>Favorites</h2>
+          <p>Pick favorites of season</p>
+          <div
+            className='chooseSeason'
+            onChange={(event) => handleSeasonChange(event)}>
+            <input
+              type='radio'
+              name='season'
+              id='winter'
+              value='winter'></input>
+            <label htmlFor='winter'>Winter</label>
+            <input
+              type='radio'
+              name='season'
+              id='spring'
+              value='spring'></input>
+            <label htmlFor='spring'>Spring</label>
+            <input
+              type='radio'
+              name='season'
+              id='summer'
+              value='summer'></input>
+            <label htmlFor='summer'>Summer</label>
+            <input
+              type='radio'
+              name='season'
+              id='autumn'
+              value='autumn'></input>
+            <label htmlFor='autumn'>Autumn</label>
+          </div>
+          <p>It's {season}</p>
         </Section>
         <Section className='coffeeShop' id='coffeeShop'>
           <h2>Coffee shop</h2>
           <p>
             In our library, we have of cozy coffee shop, welcoming in customers
-            with frothy cappuccinos and friendly conversation. you can get a
+            with frothy cappuccinos and friendly conversation. You can get a
             favorite book and read in coffee shop. Our barista to cook you best
             coffee, and also you can try desserts from bakery.
           </p>
-          <h3>Coffee & Tea</h3>
-          <ul>
-            <li>Cold Brew Coffee</li>
-            <li>French Press Pot</li>
-            <li>Espresso</li>
-            <li>Cappuccino</li>
-            <li>Hot tea</li>
-            <li>Cold tea</li>
-          </ul>
-          <h3>Desserts & Cakes </h3>
-          <ul>
-            <li>Cold Brew Coffee</li>
-            <li>French Press Pot</li>
-            <li>Espresso</li>
-            <li>Cappuccino</li>
-            <li>Hot tea</li>
-            <li>Cold tea</li>
-          </ul>
+          <div className='foodMenu'>
+            <div className='drinks'>
+              <h3>Coffee & Tea</h3>
+              <ul>
+                <li>
+                  Cold Brew Coffee <span>$3</span>
+                </li>
+                <li>
+                  French Press Pot <span>$5</span>
+                </li>
+                <li>
+                  Espresso <span>$2</span>
+                </li>
+                <li>
+                  Cappuccino <span>$4</span>
+                </li>
+                <li>
+                  Hot tea <span>$2</span>
+                </li>
+                <li>
+                  Cold tea <span>$2</span>
+                </li>
+              </ul>
+            </div>
+            <div className='desserts'>
+              <h3>Desserts & Cakes </h3>
+              <ul>
+                <li>
+                  Forest Gateau <span>$16</span>
+                </li>
+                <li>
+                  Blue Moon <span>$16</span>
+                </li>
+                <li>
+                  Truffle Cake <span>$15</span>
+                </li>
+                <li>
+                  Chocolate Cake <span>$18</span>
+                </li>
+                <li>
+                  Poet's Dream Cake <span>$16</span>
+                </li>
+                <li>
+                  Pineapple Cake <span>$14</span>
+                </li>
+              </ul>
+            </div>
+          </div>
         </Section>
         <Section className='contacts' id='contacts'>
           <h2>Our Contacts</h2>
-          {/* <iframe
+          <h3>For all Library inquiries:</h3>
+          <p>
+            Please call (617) 730-2370
+            <br /> For TTY service, please call (617)730-2370 <br />
+            Putterham Library: (617) 730-2385
+          </p>
+          <h3>Senior Staff</h3>
+          <p>
+            Library Director: <span>Amanda Hirst</span>
+          </p>
+          <iframe
             src='https://yandex.ru/map-widget/v1/?um=constructor%3Ac93972da89a32fd07a8439f24089948346489379cc6d0add0b735b3a7348e94e&amp;source=constructor'
             width='70%'
             height='400'
-            frameBorder='0'></iframe> */}
+            frameBorder='0'></iframe>
+
         </Section>
         <Section className='libraryCard' id='libraryCard'>
           <h2>Digital Library Cards</h2>
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import IconBurger from "../assets/HeaderBurgerMenu.svg";
 import IconProfile from "../assets/HeaderProfileIcon.svg";
 import IconBurgerClose from "../assets/IconBurgerClose.svg";
@@ -6,28 +6,62 @@ import * as classes from "./Header.module.css";
 
 export function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(navigator.maxTouchPoints > 0);
+  const [profileMenuOpen, setProfileMenuOpen] = useState(false);
 
   function handleBurgerClick() {
     setMenuOpen((prev) => !prev);
   }
 
+  function handleClickProfileIcon() {
+    setProfileMenuOpen((prev) => !prev);
+  }
+
+  useLayoutEffect(() => {
+    if (!isMobile) {
+      setMenuOpen(true);
+    }
+  }, []);
+
   return (
-    <header className={classes.header}>
-      <h1>Brooklyn Public Library</h1>
-      <div className={classes.menu}>
-        <img src={IconProfile} alt='Profile' className={classes.iconProfile} />
-        <div
-          className={classes.iconBurgerContainer}
-          onClick={handleBurgerClick}>
-          {menuOpen ? (
-            <img src={IconBurgerClose} alt='Burger' className={classes.icon} />
-          ) : (
-            <img src={IconBurger} alt='Menu' className={classes.iconBurger} />
-          )}
-        </div>
+    <header className={classes.header} id='headerAnchor'>
+      <a href='headerAnchor'>
+        <h1>Brooklyn Public Library</h1>
+      </a>
+      <div className={`${classes.menu} ${!isMobile && classes.profileOrder}`}>
+        <img
+          src={IconProfile}
+          alt='Profile'
+          className={classes.iconProfile}
+          onClick={handleClickProfileIcon}
+        />
+        {profileMenuOpen && (
+          <div className={classes.profileMenuOpen}>
+            <h3>Profile</h3>
+            <p>Log In</p>
+            <p>Register</p>
+          </div>
+        )}
+        {isMobile && (
+          <div
+            className={classes.iconBurgerContainer}
+            onClick={handleBurgerClick}>
+            {menuOpen ? (
+              <img
+                src={IconBurgerClose}
+                alt='Burger'
+                className={classes.icon}
+              />
+            ) : (
+              <img src={IconBurger} alt='Menu' className={classes.iconBurger} />
+            )}
+          </div>
+        )}
       </div>
       {menuOpen && (
-        <div className={classes.burgerMenu}>
+        <div
+          className={isMobile ? classes.burgerMenu : classes.burgerMenuDesktop}>
+
           <nav>
             <ul>
               <li>
